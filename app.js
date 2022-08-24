562ac5a5bf2fdb127717db4c5de21298106d45f8
ionelabrinza <ionela.brinza@yahoo.com>
2022-08-24 15:04:52 -0500

    show next,previous and random person

--- app.js
+++ app.js
@@ -40,14 +40,46 @@ const nextBtn = document.querySelector('.next-btn')
 const randomBtn = document.querySelector('.random-btn')
 
 //set starting items
-let currentItem = 2;
+let currentItem = 0;
 
 // load initial item
 
 window.addEventListener('DOMContentLoaded', function() {
-    const item = reviews[currentItem];
+    showPerson(currentItem);
+});
+
+// show person based on item
+
+function showPerson(person) {
+    const item = reviews[person];
     img.src = item.img;
     author.textContent = item.name;
     job.textContent = item.job;
     info.textContent = item.text;
+}
+
+// show next person
+
+nextBtn.addEventListener('click', function() {
+    currentItem++
+    if (currentItem > reviews.length - 1) {
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+})
+
+prevBtn.addEventListener('click', function() {
+    currentItem--
+    if (currentItem < 0) {
+        currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+})
+
+// show random person
+
+randomBtn.addEventListener("click", function() {
+    currentItem = Math.floor(Math.random() * reviews.length);
+    console.log(currentItem);
+    showPerson(currentItem);
 });
